e11ad4d99ad72686021e7412870b2ad89acd5a91
Neukz <kacperneumann1@gmail.com>
2025-08-05 20:24:09 +0200

    Handle editing event start and end datetime

--- cmd/edit.go
+++ cmd/edit.go
@@ -52,6 +52,33 @@ func updateEventFromFlags(event *cal.Event, flags *pflag.FlagSet) {
 		event.Summary = title
 	}
 
+	tz, _ := flags.GetString("tz")
+	start, _ := flags.GetString("start")
+	if start != "" {
+		startRFC, err := toRFC3339(start, tz)
+		if err != nil {
+			log.Fatalf("Invalid --start: %v", err)
+		}
+
+		event.Start.DateTime = startRFC
+		event.Start.TimeZone = tz
+	}
+
+	end, _ := flags.GetString("end")
+	if end != "" {
+		endRFC, err := toRFC3339(end, tz)
+		if err != nil {
+			log.Fatalf("Invalid --end: %v", err)
+		}
+
+		event.End.DateTime = endRFC
+		event.End.TimeZone = tz
+	}
+
+	if tz != "" && start == "" && end == "" {
+		fmt.Println("--tz provided without --start and/or --end. Time zone will not be updated.")
+	}
+
 	desc, _ := flags.GetString("desc")
 	if desc != "" {
 		event.Description = desc
@@ -66,8 +93,11 @@ func updateEventFromFlags(event *cal.Event, flags *pflag.FlagSet) {
 func init() {
 	editCmd.Flags().String("id", "", "event ID (required)")
 	editCmd.Flags().String("title", "", "event title")
+	editCmd.Flags().String("start", "", "start time, e.g. 2025-07-12 13:00")
+	editCmd.Flags().String("end", "", "end time, e.g. 2025-07-12 14:00")
 	editCmd.Flags().String("desc", "", "event description")
 	editCmd.Flags().String("loc", "", "event location")
+	editCmd.Flags().String("tz", "", "time zone (IANA name, e.g. Europe/Warsaw) — this only applies if '--start' and/or '--end' are specified, defaults to system time zone")
 	editCmd.Flags().String("cal", "", "name of the calendar to edit the event from, defaults to primary")
 
 	editCmd.MarkFlagRequired("id")
